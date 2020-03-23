e69610572d0caa486813458a397e3a07e6c19a52
Jonathan C. Warfield <jon.warfield@robotsandpencils.com>
2020-03-23 14:04:29 -0400

    password mismatch alert

---
+++ client/src/actions/alert.js
@@ -0,0 +1,12 @@
+import * as uuid from 'uuid';
+import { SET_ALERT, REMOVE_ALERT, REMOVER_ALERT } from './types';
+
+export const setAlert = (msg, alertType) => dispatch => {
+  const id = uuid.v4();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id }
+  });
+
+  setTimeout(() => dispatch({ type: REMOVER_ALERT, payload: id }), 5000);
+};

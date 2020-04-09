09eb9adac9bca81fc606c8de9c9d322971063677
Jonathan C. Warfield <jon.warfield@robotsandpencils.com>
2020-04-09 17:53:52 -0400

    dashboard logout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,8 +8,14 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' /> <span className='hide-sm' />
+          Dashboard
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
-          <i className='fas fa-sign-out-alt'></i>{' '}
+          <i className='fas fa-sign-out-alt' />{' '}
           <span className='hide-small'>Logout</span>
         </a>
       </li>

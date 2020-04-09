2b019d30387fd3ef0270f37dc2c7994b6d3a649e
Jonathan C. Warfield <jon.warfield@robotsandpencils.com>
2020-04-09 18:46:19 -0400

    bio

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,11 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({
-  getCurrentProfile,
-  auth: { user },
-  profile: { profile, loading }
-}) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
@@ -17,33 +13,34 @@ const Dashboard = ({
   return loading && profile === null ? (
     <Spinner />
   ) : (
-      <Fragment>
-        <h1 className='large text-primary'>Dashboard</h1>
-        <p className="lead">
-          <i className="fa fa-user" /> Welcome {user && user.name}
-        </p>
-        {profile !== null ? (
-          <Fragment>has</Fragment>
-        ) : (
-            <Fragment>
-              <p>You have not yet set up a profile. Please set one up.</p>
-              <Link to='/create-profile' className="btn btn-primary my-1">
-                Create Profile
-           </Link>
-            </Fragment>)}
-      </Fragment>
-    );
+    <Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fa fa-user' /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet set up a profile. Please set one up.</p>
+          <Link to='/create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
-  profile: state.profile
+  profile: state.profile,
 });
 
 export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
